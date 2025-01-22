6347cc701d74691933be95d722990eacd6be5442
deborah lu <76150275+deberinoo@users.noreply.github.com>
2025-01-22 20:12:20 +0800

    level 2: add, list

--- src/main/java/Juno.java
+++ src/main/java/Juno.java
@@ -1,20 +1,34 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Juno {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        List<String> arr = new ArrayList<>();
+
         System.out.println(greet());
 
         while (true) {
+            System.out.print("You: ");
             String input = sc.nextLine();
 
-            if (input.equals("bye")) {
+            if (input.equalsIgnoreCase("bye")) {
                 System.out.println(exit());
                 break;
+            } else if (input.equalsIgnoreCase("list")) {
+                if (arr.isEmpty()) {
+                    System.out.println("Juno: The list is empty.");
+                } else {
+                    System.out.println("Juno: Here's what you've added:");
+                    for (int i = 0; i < arr.size(); i++) {
+                        System.out.println((i + 1) + ". " + arr.get(i));
+                    }
+                }
             } else {
-                System.out.println(input);
+                arr.add(input);
+                System.out.println("added: " + input);
             }
         }
+        sc.close();
     }
 
     public static String greet() {
