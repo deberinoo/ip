38afa1cca4f12faead65a24ae4f8b6d0678980f4
Deborah Lu <deborahluyx@gmail.com>
2025-01-22 17:31:09 +0800

    level 0: rename, greet, exit

---
+++ src/main/java/Juno.java
@@ -0,0 +1,14 @@
+public class Juno {
+    public static void main(String[] args) {
+        System.out.println(greet());
+        System.out.println(exit());
+    }
+
+    public static String greet() {
+        return "Hello! I'm Juno. What can I do for you?";
+    }
+
+    public static String exit() {
+        return "Goodbye! Hope to see you again soon!";
+    }
+}
